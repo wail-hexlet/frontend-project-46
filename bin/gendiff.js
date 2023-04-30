26784a4774862b6b52422b97dc48351b11b40d72
Anatoliy Tupitsin <hexlet@wailbox.ru>
2023-04-30 21:21:39 +0300

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+
 const program = new Command();
 
 program
-.name('gendiff')
-.usage('[options]')
-.description('Compares two configuration files and shows a difference.')
-.option('-h, --help', 'display help for command')
-.version('1.0.0', '-V, --version', 'output the version number');
+  .name('gendiff')
+  .usage('[options]')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    console.log(`file1:${filepath1}`, `file2:${filepath2}`, `options:${options.format}`);
+  });
 
-program.command('gendiff');
 program.parse();
