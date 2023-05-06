eeabbbc837b219b176ab1cd09686263f9ac2f29e
Anatoliy Tupitsin <hexlet@wailbox.ru>
2023-05-06 21:02:51 +0300

    eighth step

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,12 @@
 import plain from './plain.js';
 import stylish from './stylish.js';
+import json from './json.js';
 
 export default (option, data) => {
   switch (option) {
     case 'plain': return plain(data);
     case 'stylish': return stylish(data);
+    case 'json': return json(data);
     default: throw new Error('Unknown format');
   }
 };
