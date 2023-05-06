bfc298eeb6da57f007d4a88795fe03ab6daa6ed5
Anatoliy Tupitsin <hexlet@wailbox.ru>
2023-05-06 22:00:30 +0300

    changes for hexlet tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,17 +9,22 @@ const __dirname = path.dirname(__filename);
 const getPath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 const readFile = (fileName) => readFileSync(getPath(fileName), 'utf-8');
 
-describe('gendiff', () => {
-  const fileTypes = ['stylish', 'plain', 'json'];
-  const fileExtensions = ['json', 'yaml', 'yml'];
+function makeTest(format, extension) {
+  const expectedResult = readFile(`expected_${format}.txt`);
+  test(`compare ${format} ${extension}`, () => {
+    const result = gendiff(getPath(`file1.${extension}`), getPath(`file2.${extension}`), format);
+    expect(result).toEqual(expectedResult);
+  });
+}
 
-  for (let iT = 0; iT < fileTypes.length; iT += 1) {
-    const expectedResult = readFile(`expected_${fileTypes[iT]}.txt`);
-    for (let iE = 0; iE < fileExtensions.length; iE += 1) {
-      test(`compare ${fileTypes[iT]} ${fileExtensions[iE]}`, () => {
-        const result = gendiff(getPath(`file1.${fileExtensions[iE]}`), getPath(`file2.${fileExtensions[iE]}`), fileTypes[iT]);
-        expect(result).toEqual(expectedResult);
-      });
-    }
-  }
+describe('gendiff', () => {
+  makeTest('stylish', 'json');
+  makeTest('plain', 'json');
+  makeTest('json', 'json');
+  makeTest('stylish', 'yaml');
+  makeTest('plain', 'yaml');
+  makeTest('json', 'yaml');
+  makeTest('stylish', 'yml');
+  makeTest('plain', 'yml');
+  makeTest('json', 'yml');
 });
